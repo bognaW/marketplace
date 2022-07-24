046fe761f2a4702c835fa970c7a39450e536f2d8
Bogna Wierzbinska <bogna8@gmail.com>
2022-07-24 19:27:47 +0200

    Create Spinner, Add Spinner to PrivateRoute

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Navbar from './components/Navbar';
 import Offers from './pages/Offers';
+import PrivateRoute from './components/PrivateRoute';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import ForgotPassword from './pages/ForgotPassword';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Explore />} />
         <Route path='/offers' element={<Offers />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile' element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/frogot-password' element={<ForgotPassword />} />
